1977499f89c78712c4b0b67b67ccbed4a32fd2d4
SmaliAYA <smali.aya@ensam-casa.ma>
2026-06-17 01:23:05 +0100

    upd

--- backend/database/migrations/2026_06_06_141236_create_products_table.php
+++ backend/database/migrations/2026_06_06_141236_create_products_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+   public function up(): void
+{
+    if (!Schema::hasTable('products')) {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -26,7 +27,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
+}
     /**
      * Reverse the migrations.
      */
