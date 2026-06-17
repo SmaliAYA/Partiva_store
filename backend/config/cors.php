546d5524fd1d0d05842e0c4fe60f4a203dfe4947
SmaliAYA <smali.aya@ensam-casa.ma>
2026-06-17 03:18:59 +0100

    allow all origins cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,13 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-  'allowed_origins' => [
-    'http://localhost:5173',
-    'https://partiva-store-u2zo.vercel.app',
-    'http://127.0.0.1:5173',
-    'http://localhost:5174',
-
-],
+  'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
